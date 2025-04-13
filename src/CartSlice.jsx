776311470489ee4ef13e5a9c3a8bf6630fdd7076
Kókai László <kokai.laszlo@gmail.com>
2025-04-13 19:33:54 +0200

    Task 2

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,13 +8,36 @@ export const CartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
       const { payload: item } = action;
-      state.items.push(item);
+      const elementExists = state.items.find(i => i.name === item.name);
+
+      if (elementExists) {
+        elementExists.quantity++;
+      } else {
+        state.items.push({
+          name: item.name, 
+          image: item.image,
+          cost: item.cost, 
+          quantity: 1 
+        });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { payload: item } = action;
+      const element = state.items.find(i => i.name === item.name);
+      if (element) {
+        state.items.quantity = item.quantity;
+      }
+      /*
+      To create this function, start by extracting the item's name and amount from the action.payload.
+      Then, look for the item in the state.items array that matches the extracted name. 
+      If the item is found, update its quantity to the new amount provided in the payload. 
+      This ensures the item’s quantity is correctly updated based on the action.
+      */
+  
+  
     },
   },
 });
